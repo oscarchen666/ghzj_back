1831d56f9a39c86187b49fd5a9e18c345f2abe7e
oscarcyt <9390819+oscarcyt@user.noreply.gitee.com>
2023-09-01 16:41:09 +0800

    路径修改

--- server.py
+++ server.py
@@ -359,11 +359,11 @@ def image(imgid,imgtype):
     # pppath = "../../../jiaailing/data/ChinesePainting/seals/{}"
     # hxpath = "../../../jiaailing/data/ChinesePainting/seals_sslib_qiepian/{}"
     # hzpath = "../../../jiaailing/data/ChinesePainting/juan_changtu_height1000_chang9000yishang/{}"
-    jtpath = "/home/ecs-user/data/ChinesePainting/yinzhang/{}"
+    jtpath = "/home/ecs-user/data/Chinese_Painting/yinzhang/{}"
     pppath = "/home/ecs-user/data/Chinese_Painting/seal_128/{}"
-    hxpath = "/home/ecs-user/data/ChinesePainting/seals_sslib_qiepian_300/{}"
-    hzpath = "/home/ecs-user/data/ChinesePainting/juan_changtu_yuantu/{}"
-    xtpath = "/home/ecs-user/data/ChinesePainting/juan_seamcarving_changtu_seamcarving_4500x2_1000_fenge/{}"
+    hxpath = "/home/ecs-user/data/Chinese_Painting/seals_sslib_qiepian_300/{}"
+    hzpath = "/home/ecs-user/data/Chinese_Painting/juan_changtu_yuantu{}"
+    xtpath = "/home/ecs-user/data/Chinese_Painting/juan_seamcarving_changtu_seamcarving_4500x2_1000_fenge/{}"
     if imgtype=="截图" :
         fullpath = jtpath.format(imgid)
         print(fullpath)
